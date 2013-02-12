949dabf4c909e86f0338c6bab0412401794d0aed
Richard Burton <burtonic@gmail.com>
2013-02-12 16:53:48 -0500

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Richard Burton on 2/12/13.
+//  Copyright (c) 2013 Richard Burton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
